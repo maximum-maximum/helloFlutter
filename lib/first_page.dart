af68fe4916bcc0c50716e08406c7b0afc629b2f6
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-10-13 10:21:46 +0900

    add ListView

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,49 +1,23 @@
 import 'package:flutter/material.dart';
-import 'second_page.dart';
+// import 'second_page.dart';
 
 class FilrstPage extends StatelessWidget {
-  String nameText = '';
+  final List<String> entries = <String>['A', 'B', 'C'];
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("first"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network(
-                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
-              TextField(
-                onChanged: (text) {
-                  nameText = text;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  //
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SecondPage(nameText),
-                        // fullscreenDialog: true
-                      ));
-                },
-                child: const Text('next page'),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text("list"),
         ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Center(
+                  child: Text(
+                'Entry ${entries[index]}',
+                style: TextStyle(fontSize: 30),
+              ));
+            }));
   }
 }
